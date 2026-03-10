c45186e7b489772dbc073bfd8eb112632b2c73ef
vedant-kayandekar <vedantassociates2004@gmail.com>
2026-03-10 22:57:51 +0530

    chnagesss

--- base/views.py
+++ base/views.py
@@ -1,3 +1,84 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
+import random, time, json
+from .models import RoomMember
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
+import logging
+
+def getToken(request):
+    appId = 'bb70fcdb59ee4abca8258d9dc2fc265c'
+    appCertificate = 'cd9e82ca165a46cc9ffb240ab3c419b0'
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+    
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
+def lobby(request):
+    return render(request, 'base/lobby.html')
+
+def room(request):
+    return render(request, 'base/room.html')
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name'],
+    )
+    return JsonResponse({'name': data['name']}, safe=False)
+
+
+
+
+logger = logging.getLogger(__name__)
+
+def getMember(request):
+    uid = request.GET.get('UID') or request.GET.get('uid')
+    room_name = request.GET.get('room_name')
+
+    logger.debug(f"UID: {uid}, Room Name: {room_name}")
+
+    if not uid or not room_name:
+        logger.error("Missing parameters")
+        return JsonResponse({'error': 'Missing parameters'}, safe=False, status=400)
+
+    try:
+        member = RoomMember.objects.get(
+            uid=uid,
+            room_name=room_name,
+        )
+        return JsonResponse({'name': member.name}, safe=False)
+    except RoomMember.DoesNotExist:
+        logger.error(f"Member with UID: {uid} and Room Name: {room_name} does not exist.")
+        return JsonResponse({'error': 'Member does not exist'}, safe=False, status=404)
+
+
+@csrf_exempt
+def deleteMember(request):
+    data = json.loads(request.body)
+
+    try:
+        member = RoomMember.objects.get(
+            name=data['name'],
+            uid=data['UID'],
+            room_name=data['room_name'],
+        )
+        member.delete()
+        return JsonResponse('Member was deleted', safe=False)
+    except ObjectDoesNotExist:
+        return JsonResponse('Member does not exist', safe=False, status=404)
+
+
+
 # from django.shortcuts import render
 # from django.http import JsonResponse
 # from agora_token_builder import RtcTokenBuilder
@@ -64,43 +145,10 @@
 
 #     return JsonResponse('Member was deleted',safe=False)
 
-from django.shortcuts import render
-from django.http import JsonResponse
-from agora_token_builder import RtcTokenBuilder
-import random, time, json
-from .models import RoomMember
-from django.views.decorators.csrf import csrf_exempt
-from django.core.exceptions import ObjectDoesNotExist
-import logging
 
-def getToken(request):
-    appId = 'bb70fcdb59ee4abca8258d9dc2fc265c'
-    appCertificate = 'cd9e82ca165a46cc9ffb240ab3c419b0'
-    channelName = request.GET.get('channel')
-    uid = random.randint(1, 230)
-    expirationTimeInSeconds = 3600 * 24
-    currentTimeStamp = time.time()
-    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
-    role = 1
-    
-    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
-    return JsonResponse({'token': token, 'uid': uid}, safe=False)
 
-def lobby(request):
-    return render(request, 'base/lobby.html')
 
-def room(request):
-    return render(request, 'base/room.html')
 
-@csrf_exempt
-def createMember(request):
-    data = json.loads(request.body)
-    member, created = RoomMember.objects.get_or_create(
-        name=data['name'],
-        uid=data['UID'],
-        room_name=data['room_name'],
-    )
-    return JsonResponse({'name': data['name']}, safe=False)
 
 # def getMember(request):
 #     uid = request.GET.get('uid')
@@ -115,57 +163,6 @@ def createMember(request):
 #         return JsonResponse({'name': member.name}, safe=False)
 #     except ObjectDoesNotExist:
 #         return JsonResponse({'error': 'Member does not exist'}, safe=False, status=404)
-
-
-logger = logging.getLogger(__name__)
-
-def getMember(request):
-    uid = request.GET.get('UID') or request.GET.get('uid')
-    room_name = request.GET.get('room_name')
-
-    logger.debug(f"UID: {uid}, Room Name: {room_name}")
-
-    if not uid or not room_name:
-        logger.error("Missing parameters")
-        return JsonResponse({'error': 'Missing parameters'}, safe=False, status=400)
-
-    try:
-        member = RoomMember.objects.get(
-            uid=uid,
-            room_name=room_name,
-        )
-        return JsonResponse({'name': member.name}, safe=False)
-    except RoomMember.DoesNotExist:
-        logger.error(f"Member with UID: {uid} and Room Name: {room_name} does not exist.")
-        return JsonResponse({'error': 'Member does not exist'}, safe=False, status=404)
-
-
-@csrf_exempt
-def deleteMember(request):
-    data = json.loads(request.body)
-
-    try:
-        member = RoomMember.objects.get(
-            name=data['name'],
-            uid=data['UID'],
-            room_name=data['room_name'],
-        )
-        member.delete()
-        return JsonResponse('Member was deleted', safe=False)
-    except ObjectDoesNotExist:
-        return JsonResponse('Member does not exist', safe=False, status=404)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
